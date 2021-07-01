8d6ba77e4e8204d22db00a0ac1792da627778cbb
galvizlaura69 <galvizlaura69@gmail.com>
2021-07-01 12:46:15 -0500

    1 file modified

--- src/pages/prologo.js
+++ src/pages/prologo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Prologo() {
   return (
-    <div className="p-3 ">
+    <div className="p-3 bg-dark ">
       <h1>Prologo</h1>
       <p>aiudaaa</p>
     </div>
